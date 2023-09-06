ac6dd2fd3e31bd5e0995d1043d9131acb5533b48
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-09-06 15:31:06 -0300

    feat(Variáveis e Strings.js):função remove parênteses - @claudionsc - @esterarruda

--- Variáveis e Strings/Variáveis e Strings.js
+++ Variáveis e Strings/Variáveis e Strings.js
@@ -19,14 +19,27 @@ const pokemon = "pikachu, 3, eletric, (thunderShock - QUICK attack - thuNder), S
 
 const sliceWords =  pokemon.split(",");
 const capitalize = s => s && s[0].toUpperCase() + s.slice(1);
-const ability = sliceWords[3].split('-')
+
+const attacks = sliceWords[3].split('-')
+
+attacks[0].slice(1, -1)
+
+const primeira = attacks[0].replace("(", " ")
+
+const removeParenteses = (p, arr) => {
+    arr[p].slice(1, -1)
+    s = s[0].replace("(", " ")
+    s = s[s.length - 1].replace(")", " ")
+    // console.log(s[0])
+    return s;
+}
 
 const obj ={
     "name": capitalize(sliceWords[0]),
     "lv": sliceWords[1],
     "type": sliceWords[2].toUpperCase(),
-    // "ability": 
-    // "Attacks": [sliceWords[],]
+    "ability": sliceWords[4].toLowerCase(),
+    // "attacks": removeParenteses(attacks),
 
     // "Places":
     //     - veridian
@@ -35,5 +48,5 @@ const obj ={
 }
 
 
-console.log(ability);
+console.log(removeParenteses(pokemon[3]));
 // console.log(sliceWords);
