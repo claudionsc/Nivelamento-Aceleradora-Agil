d66edfed4da08558b8f266fb8f9d3c59287e2737
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-09-14 13:47:39 -0300

    feat(exerc_01.js): continuando exercícios da folha 03 (Exerc. 1.3 - 1.6) - @claudionsc - @ester-Arruda

--- Condicionais/folha_03/exerc_01.js
+++ Condicionais/folha_03/exerc_01.js
@@ -9,6 +9,8 @@ const somarValores = (n1, n2) => {
 
 // somarValores(30, 21)
 
+// Exerc. 1.2
+
 const calcularValorVenda = (valorUnitario, quantidade) => {
     const valorTotalVenda = quantidade * valorUnitario
     if(valorTotalVenda > 100) {
@@ -18,3 +20,56 @@ const calcularValorVenda = (valorUnitario, quantidade) => {
     console.log(`Valor total da venda ${valorTotalVenda},00`)
 }
 // calcularValorVenda(6, 100)
+
+// Exerc. 1.3
+
+const ePar = numero => {
+    if(numero % 2 === 0){
+        return console.log(`O número ${numero} é par`)
+    } else {
+        return console.log(`O número ${numero} é ímpar`)
+    }
+}
+
+// ePar(44)
+
+// Exerc. 1.4
+
+const positivoOuNegativo = numero => {
+    if(numero < 0){
+        return console.log(`O número ${numero} é negativo`)
+    } else if(numero > 0){
+        return console.log(`O número ${numero} é positivo`)
+    } else if(numero == 0 || !numero){
+        return console.log(`O número ${numero} é nulo`)
+    }
+}
+
+// positivoOuNegativo(1)
+
+// Exerc. 1.5
+
+const somaCondicional = (n1, n2) => {
+    const soma = n1 + n2
+
+    if(soma > 20){
+        return console.log(soma + 8)
+    } else if(soma <= 20){
+        return console.log(soma - 8)
+    }
+}
+
+// somaCondicional(10, 11)
+
+// Exerc. 1.6
+
+const raizCondicional = numero => {
+
+    if(numero >= 0){
+         console.log(Math.sqrt(numero))
+    } else {
+         console.log(numero**2)
+    }
+}
+
+// raizCondicional(-3)
