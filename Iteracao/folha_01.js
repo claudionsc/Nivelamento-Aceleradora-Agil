e081ca5960de396de25a1b0868165692bf9adace
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-09-22 13:23:53 -0300

    feat(iteracao/folha_01.js): resolucao folha 1 (exercício 26)  - @ester-Arruda - @Claudionsc

--- Iteracao/folha_01.js
+++ Iteracao/folha_01.js
@@ -108,10 +108,10 @@ const mostraMaiorMenorNum = (...num) => {
 
 const tresMaioresDigitados = (...num) => {
   let ordenados = num.sort((a, b) => {
-    return b - a
-  })
-  console.log(ordenados[0], ordenados[1], ordenados[2])
-}
+    return b - a;
+  });
+  console.log(ordenados[0], ordenados[1], ordenados[2]);
+};
 // tresMaioresDigitados(3, 4, 1, 8, 2, 9, 5)
 
 // Exerc. 1.10
@@ -142,109 +142,116 @@ const lerPares = (...pares) => {
 // Exerc. 1.11
 
 const contaNumeros = (...num) => {
-    let qtdadeNum = 0
-    for(let i = 0; i < num.length; i++) {
-        if(num[i] == -1) {
-            break
-        }
-        qtdadeNum++
+  let qtdadeNum = 0;
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] == -1) {
+      break;
     }
-    console.log(qtdadeNum)
-}
+    qtdadeNum++;
+  }
+  console.log(qtdadeNum);
+};
 // contaNumeros(2, 4, 7, 7, 4, 3, -1, 4, 7, 9)
 
 // Exerc. 1.12
 
 const contaNumerosESoma = (...num) => {
-    let somaNumeros = []
-    for(let i = 0; i < num.length; i++) {
-        if(num[i] == -1) {
-            break
-        } else {
-            somaNumeros.push(num[i])
-        }
+  let somaNumeros = [];
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] == -1) {
+      break;
+    } else {
+      somaNumeros.push(num[i]);
     }
-    qtdadeNum = somaNumeros.reduce((acum, valor) => { return acum + valor })
-    console.log(qtdadeNum)
-}
+  }
+  qtdadeNum = somaNumeros.reduce((acum, valor) => {
+    return acum + valor;
+  });
+  console.log(qtdadeNum);
+};
 // contaNumerosESoma(2, 4, 7, 7, 4, 3, -1, 4, 7, 9)
 
 // Exerc. 1.13
 
 const retornarParesImpares = (...num) => {
-    let numSelecionados = []
-    for(let i = 0; i < num.length; i++) {
-        if(num[i] == -1) {
-            break
-        } else {
-            numSelecionados.push(num[i])
-        }
+  let numSelecionados = [];
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] == -1) {
+      break;
+    } else {
+      numSelecionados.push(num[i]);
     }
+  }
 
-    let impares = numSelecionados.filter((valor) => { return valor % 2 == 0 })
-    let pares = numSelecionados.filter((valor) => { return valor % 2 != 0 })
+  let impares = numSelecionados.filter((valor) => {
+    return valor % 2 == 0;
+  });
+  let pares = numSelecionados.filter((valor) => {
+    return valor % 2 != 0;
+  });
 
-    console.log(impares, pares)
-}
+  console.log(impares, pares);
+};
 // retornarParesImpares(2, 4, 7, 7, 4, 3, -1, 4, 7, 9)
 
 // Exerc. 1.14
 const retornarMultiplos = (...num) => {
-    let numSelecionados = []
-    for(let i = 0; i < num.length; i++) {
-        if(num[i] == -1) {
-            break
-        } else {
-            numSelecionados.push(num[i])
-        }
+  let numSelecionados = [];
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] == -1) {
+      break;
+    } else {
+      numSelecionados.push(num[i]);
     }
+  }
 
-    let a = numSelecionados.filter((valor) => {
-        if(valor % 2 === 0 && valor % 5 === 0) { return valor }
-    })
+  let a = numSelecionados.filter((valor) => {
+    if (valor % 2 === 0 && valor % 5 === 0) {
+      return valor;
+    }
+  });
 
-    let b = numSelecionados.filter((valor) => {
-        if(valor % 2 === 0 && valor % 3 === 0) { return valor }
-    })
+  let b = numSelecionados.filter((valor) => {
+    if (valor % 2 === 0 && valor % 3 === 0) {
+      return valor;
+    }
+  });
 
-    console.log(`${a.length}, ${b.length}`)
-}
+  console.log(`${a.length}, ${b.length}`);
+};
 // retornarMultiplos(10, 12, 1, 7, 30, 18, -1, 4, 40, 9)
 
 // Exerc. 1.15
 
 const lerNumeros = (...num) => {
+  let pares = 0;
+  let impares = 0;
+  let mediaPares = 0;
+  let mediaGeral = 0;
 
-    
-    let pares = 0
-    let impares = 0
-    let mediaPares = 0
-    let mediaGeral = 0
-
-    for(let i = 0; i < num.length; i++){
-        if(num[i] === 0){
-            break
-        } else{
-            if(num[i] % 2 == 0){
-                pares++
-                mediaPares = (mediaPares + num[i]) / pares
-            }
-            if(num[i] % 2 !== 0){
-                impares++
-            }
-            mediaGeral += num[i]
-        }
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] === 0) {
+      break;
+    } else {
+      if (num[i] % 2 == 0) {
+        pares++;
+        mediaPares = (mediaPares + num[i]) / pares;
+      }
+      if (num[i] % 2 !== 0) {
+        impares++;
+      }
+      mediaGeral += num[i];
     }
-    mediaGeral /= num.length
+  }
+  mediaGeral /= num.length;
 
-    console.log(`
+  console.log(`
     Quantidade de pares: ${pares}
     Quantidade de ímpares: ${impares}
     Média de números pares: ${mediaPares}
     Média geral: ${mediaGeral}
-    `)
-    
-}
+    `);
+};
 
 // lerNumeros(2, 3, 4, 12, 5, 8, 3, 5, 0, 1)
 
@@ -253,62 +260,58 @@ const lerNumeros = (...num) => {
 // Exerc. 1.17
 
 const lerNome = (...dados) => {
-  let qtdadeAlunos = 0
-  for(let i = 0; i < dados.length; i+=2) {
-    if(dados[i] === 'fim'){ break }
-    console.log(`Nome: ${dados[i]}, média: ${dados[i+1]}`)
-    qtdadeAlunos++
+  let qtdadeAlunos = 0;
+  for (let i = 0; i < dados.length; i += 2) {
+    if (dados[i] === "fim") {
+      break;
+    }
+    console.log(`Nome: ${dados[i]}, média: ${dados[i + 1]}`);
+    qtdadeAlunos++;
   }
-  console.log(`Quantidade de alunos: ${qtdadeAlunos}`)
-}
+  console.log(`Quantidade de alunos: ${qtdadeAlunos}`);
+};
 // lerNome("Beto", 7, "Carol", 8, "Carina", 10, "fim", 10)
-
 
 // Exerc. 1.18
 
 const convertePolegada = () => {
-  const polegada = 2.54
-  for(i = 1; i <= 20; i++) {
-    console.log(`${i} polegada(s) em centímetros é ${i * polegada}`)
+  const polegada = 2.54;
+  for (i = 1; i <= 20; i++) {
+    console.log(`${i} polegada(s) em centímetros é ${i * polegada}`);
   }
-}
+};
 
 // convertePolegada()
 
 // Exerc. 1.19
 
-const tabuada = num => {
-
-  for(let i = 1; i <= 10; i++){
-    console.log(`${num} * ${i} = ${num*i}`)
+const tabuada = (num) => {
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${num} * ${i} = ${num * i}`);
   }
-  
-}
+};
 
 // tabuada(72)
 
 // Exerc. 1.20
 
 const quadradosPerfeitos = (...num) => {
+  let contador = 0;
+  let sqrt;
 
-  let contador = 0
-  let sqrt
-
-  for(let i = 0; i < num.length; i++){
-
-    if(num[i] === 0){
-      break
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] === 0) {
+      break;
     }
 
-      sqrt = Math.sqrt(num[i])
-    
-    if(Number.isInteger(sqrt)){
-      contador++
+    sqrt = Math.sqrt(num[i]);
+
+    if (Number.isInteger(sqrt)) {
+      contador++;
     }
   }
-  console.log(`Foram lido(s) ${contador} quadrado(s) perfeito(s)`)
-  
-}
+  console.log(`Foram lido(s) ${contador} quadrado(s) perfeito(s)`);
+};
 
 // quadradosPerfeitos(81, 25, 100, 0, 9)
 
@@ -316,114 +319,149 @@ const quadradosPerfeitos = (...num) => {
 
 const ePrimo = (number) => {
   if (number <= 1) {
-    return console.log(`${number} não é primo.`)
+    return console.log(`${number} não é primo.`);
   }
 
   if (number <= 3) {
-    return console.log(`${number} é primo.`)
+    return console.log(`${number} é primo.`);
   }
 
   // Verifica divisibilidade por 2 ou 3
   if (number % 2 === 0 || number % 3 === 0) {
-    return   console.log(`${number} não é primo.`)
+    return console.log(`${number} não é primo.`);
   }
 
   // Verifica divisibilidade por outros números ímpares
   for (let i = 5; i * i <= number; i += 6) {
-    if (number % i === 0 ||  number % (i + 2) === 0) {
-      return console.log(`${number} não é primo.`)
+    if (number % i === 0 || number % (i + 2) === 0) {
+      return console.log(`${number} não é primo.`);
     }
   }
-  return console.log(`${number} é primo.`)
-}
+  return console.log(`${number} é primo.`);
+};
 
 // ePrimo(2)
 
 // Exerc. 1.22
 
 const quociente = (dividendo, divisor) => {
-  let dividendoAlterado = dividendo
-  let contador = 0
-  while(dividendoAlterado > 0) {
-    dividendoAlterado -= divisor
-    contador++
+  let dividendoAlterado = dividendo;
+  let contador = 0;
+  while (dividendoAlterado > 0) {
+    dividendoAlterado -= divisor;
+    contador++;
   }
-  console.log(`O quociente de ${dividendo} é ${contador}`)
-}
+  console.log(`O quociente de ${dividendo} é ${contador}`);
+};
 
 // quociente(61, 5)
 
 // Exerc. 1.23
 
 const raizQuadrada = (num) => {
-  let contador = 0
-  let numero = num
-  for(let i = 1; i <= numero; i++) {
-    if(i % 2 != 0) {
-      numero -= i
-      contador++
+  let contador = 0;
+  let numero = num;
+  for (let i = 1; i <= numero; i++) {
+    if (i % 2 != 0) {
+      numero -= i;
+      contador++;
     }
   }
-  console.log(`A raiz quadrada de ${num} é ${contador}.`)
-}
+  console.log(`A raiz quadrada de ${num} é ${contador}.`);
+};
 
 // raizQuadrada(225)
 
 // Exerc. 1.24
 
 const contarNegativos = () => {
+  let a = [-1, -2, 3, -4, -5];
+  let contador = 0;
 
-  let a = [-1, -2, 3, -4, -5]
-  let contador = 0
-
-  for(let i = 0; i < a.length; i++){
-    if(a[i] < 0){
-      contador++
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] < 0) {
+      contador++;
     }
   }
 
-  console.log(`São ${contador} números negativos`)
-  
-}
+  console.log(`São ${contador} números negativos`);
+};
 
 // contarNegativos()
 
 // Exerc. 1.25
 
 const calcularSalarios = (...funcionarios) => {
+  let valorHora = 26.9;
+  let salarioBruto;
+  let salarioLiquido;
+  let masculinoS = [];
+  let femininoS = [];
 
-  let valorHora = 26.9
-  let salarioBruto
-  let salarioLiquido
-  let masculinoS = []
-  let femininoS = []
+  for (let i = 0; i < funcionarios.length; i++) {
+    salarioBruto = funcionarios[i][2] * valorHora;
 
-
-  for(let i = 0; i < funcionarios.length; i++){
-    
-    salarioBruto = funcionarios[i][2] * valorHora
-
-    if(funcionarios[i][0] === -11){
-      break
-    } else if(funcionarios[i][1] === 'M'){
-      salarioLiquido = salarioBruto - (salarioBruto * 10 / 100)
-      masculinoS.push(salarioLiquido)
-    } else if(funcionarios[i][1] === 'F'){
-      salarioLiquido = salarioBruto - (salarioBruto * 6 / 100)
-      femininoS.push(salarioLiquido)
+    if (funcionarios[i][0] === -11) {
+      break;
+    } else if (funcionarios[i][1] === "M") {
+      salarioLiquido = salarioBruto - (salarioBruto * 10) / 100;
+      masculinoS.push(salarioLiquido);
+    } else if (funcionarios[i][1] === "F") {
+      salarioLiquido = salarioBruto - (salarioBruto * 6) / 100;
+      femininoS.push(salarioLiquido);
     }
-    console.log( `
+    console.log(`
       O salário bruto do funcionário de código ${funcionarios[i][0]}, 
-      gênero ${funcionarios[i][1]} e com ${funcionarios[i][2]} horas trabalhadas é: R$ ${salarioBruto.toLocaleString()}.
-      O salário líquido resultou em R$ ${salarioLiquido.toLocaleString()}.`)
+      gênero ${funcionarios[i][1]} e com ${
+      funcionarios[i][2]
+    } horas trabalhadas é: R$ ${salarioBruto.toLocaleString()}.
+      O salário líquido resultou em R$ ${salarioLiquido.toLocaleString()}`);
   }
 
-  let mediaMasculina =  masculinoS.reduce((acumulador, valor) => { return acumulador + valor }) / 2
-  let mediaFeminina =  femininoS.reduce((acumulador, valor) => { return acumulador + valor }) / 2
+  let mediaMasculina =
+    masculinoS.reduce((acumulador, valor) => {
+      return acumulador + valor;
+    }) / masculinoS.length;
+  let mediaFeminina =
+    femininoS.reduce((acumulador, valor) => {
+      return acumulador + valor;
+    }) / femininoS.length;
   console.log(`
     A média do salário líquido feminino foi de R$ ${mediaFeminina.toLocaleString()} 
-    e do masculino foi de ${mediaMasculina.toLocaleString()}`)
-  
-}
+    e do masculino foi de ${mediaMasculina.toLocaleString()}`);
+};
 
-calcularSalarios([1, 'M', 220], [2, 'M', 205], [3, 'F', 230], [4, 'F', 215], [-11, 'M', 0])
+// calcularSalarios([1, 'M', 220], [2, 'M', 205], [3, 'F', 230], [4, 'F', 215], [-11, 'M', 0])
+
+// Exerc. 1.26
+
+const pesquisa = (...dados) => {
+  let salario = [];
+  let numFilhos = [];
+
+  for (let i = 0; i < dados.length; i++) {
+    if (dados[i][0] < 0) {
+      break;
+    }
+
+    salario.push(dados[i][0]);
+    numFilhos.push(dados[i][1]);
+  }
+
+  let salarioMedia =
+    salario.reduce((acumulador, valor) => {
+      return acumulador + valor;
+    }) / salario.length;
+  let filhosMedia =
+    numFilhos.reduce((acumulador, valor) => {
+      return acumulador + valor;
+    }) / numFilhos.length;
+  let maiorSalario = salario.sort().pop();
+    console.log(`
+      A média dos salários foi de R$ ${salarioMedia.toLocaleString()}
+      e do número de filhos foi de ${filhosMedia}.
+      O maior salário foi de ${maiorSalario}
+      `)
+};
+
+pesquisa([2000, 1], [2500, 1], [3200, 2], [4500, 3], [-1100, 0], [5000, 4]);
