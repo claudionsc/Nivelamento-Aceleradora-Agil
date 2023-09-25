620cef9505e63c2f3cea56da2d3048bbfb58e48b
Ester Arruda <earruda84@gmail.com>
2023-09-25 11:43:50 -0300

    feat(arrays/folha_01.js): resolucao folha 1 (exercícios 4 e 5) - @Ester Arruda - @Claudionsc

--- Arrays/folha_01/exerc_01.js
+++ Arrays/folha_01/exerc_01.js
@@ -29,25 +29,67 @@ const lerVetor02 = () => {
 // Exerc.03
 
 const lerVetor03 = () => {
-    const arr = [1, 2, -3, 4, -5, 6, -7, 8];
-    let positivos = []
-    let negativos = []
+  const arr = [1, 2, -3, 4, -5, 6, -7, 8]
+  let positivos = []
+  let negativos = []
 
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] < 0){
-            negativos.push(arr[i])
-            while(negativos.length < 8){
-                negativos[i]
-            }
-        } else {
-            positivos.push(arr[i])
-            while(positivos.length < 8){
-                positivos[i]
-            }
-        }
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] < 0){
+      negativos.push(arr[i])
+    } 
+    else {
+      positivos.push(arr[i])
     }
-  
-    console.log(positivos, negativos);
-  };
-  
-  lerVetor03();
+  }
+
+  if(positivos.length < 8) positivos.length = 8
+
+  if(negativos.length < 8) negativos.length = 8
+
+  console.log(positivos, negativos)
+};
+
+// lerVetor03()
+
+// Exerc.04
+
+const vetorReverso = () => {
+  let vetorReverso = vetor.reverse()
+  console.log(vetorReverso)
+}
+
+let vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
+  15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+// vetorReverso(vetor)
+
+// Exerc.05
+const vetorIntercalado = (vetorA, vetorB) => {
+  debugger
+  let arrayIntercalado = []
+
+  for(let i = 0; i < vetorA.length; i++) {
+    arrayIntercalado.push(vetorA[i])
+    for(let j = 0; j < vetorB.length; j++) {
+      if(i == j) {
+        arrayIntercalado.push(vetorB[j])
+      }
+    }
+  }
+  console.log(arrayIntercalado)
+}
+
+let vetorA = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+let vetorB = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+// vetorIntercalado(vetorA, vetorB)
+
+// Exerc.06
+
+
+
+
+
+
+
+
+
